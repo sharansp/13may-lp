9312b3434ca307d6596a1651ddfa3f5ac9e7734d
Sharangowda s patil <sharangowda2425@gmail.com>
2017-05-14 10:33:08 +0530

    Code refactoring: Seperated manifest from component.js

--- LocalPricing/WebContent/Component.js
+++ LocalPricing/WebContent/Component.js
@@ -5,69 +5,73 @@ sap.ui.define([
 
 	return UIComponent.extend("sap.ui.my.Component", {
 
-		metadata : {
-
-			"rootView" : "sap.ui.my.main.view.App", // initial view
-
-			"routing" : {
-				"config" : {
-					"routerClass" : "sap.m.routing.Router", // router class
-					"viewType" : "XML", // types of views using in app
-					"viewPath" : "sap.ui.my.main.view", // folder of views
-					"controlId" : "app", // container where pages are placed while navigating 
-					"controlAggregation" : "pages", // contents which needs to be replced while navigating
-					"transition" : "slide" // navigation transition effect
-				},
-
-				"routes" : [ { // defining routes
-					"pattern" : "", // pattern of the URL
-					"name" : "dashboard", // name of route
-					"target" : "dashboard" // name of target
-				}, 
-				{
-					"pattern" : "quotation",
-					"name" : "quotation",
-					"target" : "quotation"
-				},
-				{
-					"pattern" : "quotations",
-					"name" : "quotations",
-					"target" : "quotations"
-				},
-				{
-					"pattern" : "quotation/{id}",
-					"name" : "quotation",
-					"target" : "quotation"
-				}],
-
-				"targets" : { // defining targets
-					"dashboard" : { // route name
-						"viewName" : "Dashboard" // target view name, will be navigated to this view
-					},
-					"quotation" : {
-						"viewName" : "Quotation"
-					},
-					"quotations": {
-						"viewName" : "QuotationsList"
-					},
-					"notFound": {
-						"viewName": "NotFound",
-						"transition": "show"
-					}
-				}
-
-			}/*,
-			config: {
-                sample: {
-                    stretch: true,
-                    files: [
-                        "sap/ui/my/main/wbsmetadata.xml",
-                        "sap/ui/my/main/service/MockServer.js",
-                        "Component.js",
-                    ]
-                }
-            }*/
+		metadata: {
+			manifest: "json"
 		},
+		
+		// metadata : {
+
+		// 	"rootView" : "sap.ui.my.main.view.App", // initial view
+
+		// 	"routing" : {
+		// 		"config" : {
+		// 			"routerClass" : "sap.m.routing.Router", // router class
+		// 			"viewType" : "XML", // types of views using in app
+		// 			"viewPath" : "sap.ui.my.main.view", // folder of views
+		// 			"controlId" : "app", // container where pages are placed while navigating 
+		// 			"controlAggregation" : "pages", // contents which needs to be replced while navigating
+		// 			"transition" : "slide" // navigation transition effect
+		// 		},
+
+		// 		"routes" : [ { // defining routes
+		// 			"pattern" : "", // pattern of the URL
+		// 			"name" : "dashboard", // name of route
+		// 			"target" : "dashboard" // name of target
+		// 		}, 
+		// 		{
+		// 			"pattern" : "quotation",
+		// 			"name" : "quotation",
+		// 			"target" : "quotation"
+		// 		},
+		// 		{
+		// 			"pattern" : "quotations",
+		// 			"name" : "quotations",
+		// 			"target" : "quotations"
+		// 		},
+		// 		{
+		// 			"pattern" : "quotation/{id}",
+		// 			"name" : "quotation",
+		// 			"target" : "quotation"
+		// 		}],
+
+		// 		"targets" : { // defining targets
+		// 			"dashboard" : { // route name
+		// 				"viewName" : "Dashboard" // target view name, will be navigated to this view
+		// 			},
+		// 			"quotation" : {
+		// 				"viewName" : "Quotation"
+		// 			},
+		// 			"quotations": {
+		// 				"viewName" : "QuotationsList"
+		// 			},
+		// 			"notFound": {
+		// 				"viewName": "NotFound",
+		// 				"transition": "show"
+		// 			}
+		// 		}
+
+		// 	}/*,
+		// 	config: {
+  //               sample: {
+  //                   stretch: true,
+  //                   files: [
+  //                       "sap/ui/my/main/wbsmetadata.xml",
+  //                       "sap/ui/my/main/service/MockServer.js",
+  //                       "Component.js",
+  //                   ]
+  //               }
+  //           }*/
+		// },
 
 		init : function() {
 			
